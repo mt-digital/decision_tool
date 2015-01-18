cad0549fcfa75c04255a55da4eb510641984d0ad
Matthew Turner <maturner01@gmail.com>
2015-01-18 13:53:06 -0800

    added sketch of barchart.js with its example.json data

---
+++ barchart.js
@@ -0,0 +1,16 @@
+/**
+ * Plot bar charts comparing the region selected in add_counties.js to the 
+ * full sample region.
+ */
+
+// set up standard margins 
+
+var margin = {top: 20, right: 
+
+var svg = d3.select("body").append("svg")
+
+d3.json( "example.json", function (error, data) {
+    svg.select("    
+        
+
+}
